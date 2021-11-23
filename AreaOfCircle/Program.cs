22aa714e66ceed06cf611fd718921a8fb5519f4f
tmoore28 <mooretylor28@gmail.com>
2021-11-22 21:09:53 -0500

    first commit area of circle

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+            Console.WriteLine("Enter the radius of the circle.");
+            radius = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * radius * radius;
+            Console.WriteLine("Area of the circle with radius {0} is {1}", radius, area);
+        }
+    }
+}
